9d5c22f2ec88b89088b2a0b3da914e1ffdfc3db7
Ronald Coppieters <ronald.coppieters@student.pxl.be>
2020-06-24 16:12:44 +0200

    Added meta title for each view

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,22 +8,42 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      title: "Ronald Coppieters Portfolio"
+    }
   },
   {
     path: "/news/1",
     name: "News1",
-    component: () => import("@/views/news/Page1.vue")
+    component: () => import("@/views/news/Page1.vue"),
+    meta: {
+      title: "News | Page 1"
+    }
   },
   {
     path: "/news/2",
     name: "News2",
-    component: () => import("@/views/news/Page2.vue")
+    component: () => import("@/views/news/Page2.vue"),
+    meta: {
+      title: "News | Page 2"
+    }
   },
   {
     path: "/news/3",
     name: "News3",
-    component: () => import("@/views/news/Page3.vue")
+    component: () => import("@/views/news/Page3.vue"),
+    meta: {
+      title: "News | Page 3"
+    }
+  },
+  {
+    path: "/news/4",
+    name: "News4",
+    component: () => import("@/views/news/Page4.vue"),
+    meta: {
+      title: "News | Page 4"
+    }
   }
 ];
 
@@ -31,6 +51,12 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes
+});
+
+router.beforeEach((to, from, next) => {
+  const title = to.meta.title;
+  if (title) document.title = title;
+  next();
 });
 
 export default router;
